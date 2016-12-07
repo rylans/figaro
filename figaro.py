4b552da0f918fc4af86415daf003cf42aa56fd31
Rylan <rylans@gmail.com>
2016-12-07 15:17:20 -0800

    Basic arithmetic handling

--- figaro.py
+++ figaro.py
@@ -15,6 +15,7 @@
    limitations under the License.
 """
 from abc import abstractmethod
+from math import log, sqrt
 
 class StatementHandlerBase(object):
     """Abstract base class for handling general statements."""
@@ -52,6 +53,181 @@ class GreetingStatementHandler(StatementHandlerBase):
             return Response('Hey there.', [])
         return Response('Hello!', [])
 
+class ArithmeticHandler(StatementHandlerBase):
+    """Class for basic arithmetic responses"""
+    _ADD = lambda x, y: sum([x, y])
+    _SUBTRACT = lambda x, y: sum([x, -1*y])
+    _LOG10 = lambda x: log(x, 10)
+
+    INFIX_OPS = [('+', _ADD),
+                 ('plus', _ADD),
+                 ('-', _SUBTRACT),
+                 ('minus', _SUBTRACT)]
+
+    UNARY_OPS = [('root', sqrt),
+                 ('log', _LOG10)]
+
+    def _is_number_in(self, tokens):
+        """Return true if there is a number available
+
+        >>> ArithmeticHandler()._is_number_in('5 of them'.split(' '))
+        True
+        """
+        for token in tokens:
+            try:
+                return float(token) != None
+            except ValueError:
+                pass
+        return False
+
+    def _has_number_after(self, tokens, ix):
+        """Check if number occurs after given index
+
+        >>> ArithmeticHandler()._has_number_after("give me log of 7".split(), 3)
+        True
+
+        >>> ArithmeticHandler()._has_number_after("what is 7 log of?".split(), 3)
+        False
+        """
+        def try_parse_float(x):
+            try:
+                return float(x)
+            except ValueError:
+                return None
+        return any([try_parse_float(x) for x in tokens[ix:]])
+
+    def _has_infix(self, statement):
+        """Return true if it is able to handle infix operation
+
+        >>> ArithmeticHandler()._has_infix("calculate 5 plus 2")
+        True
+
+        >>> ArithmeticHandler()._has_infix("calculate + 2 please")
+        False
+        """
+        tokens = statement.split(' ')
+        for ix, token in enumerate(tokens):
+            for op, _ in ArithmeticHandler.INFIX_OPS:
+                if op == token:
+                    return self._is_number_in(tokens[ix:]) and self._is_number_in(tokens[:ix])
+        return False
+
+    def _has_unary(self, statement):
+        """Return true if unary statement can be handled
+
+        >>> ArithmeticHandler()._has_unary('what is square root of 5')
+        True
+
+        >>> ArithmeticHandler()._has_unary('what is square root of')
+        False
+
+        >>> ArithmeticHandler()._has_unary('root 64.0')
+        True
+
+        >>> ArithmeticHandler()._has_unary('what is 12 of 5')
+        False
+        """
+        tokens = statement.split(' ')
+        for ix, token in enumerate(tokens):
+            for op, _ in ArithmeticHandler.UNARY_OPS:
+                if op == token:
+                    return self._is_number_in(tokens[ix:])
+        return False
+
+    def _calc_unary(self, statement):
+        """Return output of unary operation
+
+        >>> ArithmeticHandler()._calc_unary('what is root of 16')
+        4.0
+
+        >>> ArithmeticHandler()._calc_unary('calculate log 100')
+        2.0
+
+        >>> ArithmeticHandler()._calc_unary('you know what square root of 64 is?')
+        8.0
+        """
+        tokens = statement.split(' ')
+        op_func = None
+        start_ix = 0
+        arg = None
+        for ix, token in enumerate(tokens):
+            for op_word, func in ArithmeticHandler.UNARY_OPS:
+                if op_word == token:
+                    op_func = func
+                    start_ix = ix
+        for token in tokens[start_ix:]:
+            try:
+                number = float(token)
+                arg = number
+                break
+            except ValueError:
+                pass
+
+        if op_func == None or arg == None:
+            raise RuntimeError("Unable to calculate unary operation: %s" % statement)
+        return op_func(arg)
+
+    def _calc_infix(self, statement):
+        """Return output of infix operation
+
+        >>> ArithmeticHandler()._calc_infix('7 + 2')
+        9.0
+
+        >>> ArithmeticHandler()._calc_infix('tell me 20 minus 48')
+        -28.0
+        """
+        tokens = statement.split(' ')
+        op_func = None
+        start_ix = 0
+        arg_a = None
+        arg_b = None
+        for ix, token in enumerate(tokens):
+            for op_word, func in ArithmeticHandler.INFIX_OPS:
+                if op_word == token:
+                    op_func = func
+                    start_ix = ix
+
+        for token in tokens[start_ix:]:
+            try:
+                number = float(token)
+                arg_b = number
+                break
+            except ValueError:
+                pass
+
+        for token in tokens[:start_ix]:
+            try:
+                number = float(token)
+                arg_a = number
+                break
+            except ValueError:
+                pass
+
+        if op_func == None or arg_a == None or arg_b == None:
+            raise RuntimeError("Unable to calculate operation: %s" % statement)
+        return op_func(arg_a, arg_b)
+
+    def can_handle(self, statement):
+        low = statement.lower()
+        return self._has_infix(low) or self._has_unary(low)
+
+    def handle(self, statement):
+        """Respond to basic arithmetic request
+
+        >>> ArithmeticHandler().handle("What is 13 - 20").answer
+        '-7.0'
+
+        >>> ArithmeticHandler().handle("Calculate for me square root of 100").answer
+        '10.0'
+        """
+        if self._has_unary(statement):
+            num = self._calc_unary(statement)
+            return Response(str(num), [])
+        elif self._has_infix(statement):
+            num = self._calc_infix(statement)
+            return Response(str(num), [])
+        raise RuntimeError("ArithmeticHandler reported ability to handle %s but can't" % statement)
+
 class Response(object):
     """Response to a question or statement
 
@@ -77,6 +253,7 @@ class Figaro(object):
     def __init__(self):
         self._handlers = []
         self._handlers.append(GreetingStatementHandler())
+        self._handlers.append(ArithmeticHandler())
         self._handlers.append(DefaultStatementHandler())
 
     def _dispatch_to_handler(self, statement):
@@ -90,6 +267,9 @@ class Figaro(object):
 
         >>> Figaro().hears("Hello there")
         'Hello!'
+
+        >>> Figaro().hears("5 minus 13")
+        '-8.0'
 
         >>> Figaro().hears("jibberjabber")
         "I'm not sure how to respond to that."
--- tests/test_figaro.py
+++ tests/test_figaro.py
@@ -4,6 +4,14 @@ def test_response_from_default_handler():
     expected = "I'm not sure how to respond to that."
     assert Figaro().hears("abcd") == expected
 
-def test_response_form_greeting_handler():
+def test_response_from_greeting_handler():
     expected = "Hey there."
     assert Figaro().hears("hey") == expected
+
+def test_response_from_math_handler():
+    expected = "-32.0"
+    assert Figaro().hears("Calculate 1 minus 33") == expected
+
+def test_response_from_math_handler_unary():
+    expected = "3.0"
+    assert Figaro().hears("whats log 1000") == expected
